4ce6223465ae0be62bfb78c9e99626f146651bb9
Justyna Pietryga <justyna.pietryga@gmail.com>
2018-10-20 18:21:55 +0200

    Refactor of FlashcardsActivity

--- app/src/main/java/com/example/justyna/flashcards/FlashcardsActivity.java
+++ app/src/main/java/com/example/justyna/flashcards/FlashcardsActivity.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,32 +14,33 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-import com.example.justyna.flashcards.databasemanager.CategoryDAO;
 import com.example.justyna.flashcards.databasemanager.VocabularyDAO;
 
+import static com.example.justyna.flashcards.FlashcardsActivity.FlashcardSide.BACK;
+import static com.example.justyna.flashcards.FlashcardsActivity.FlashcardSide.FRONT;
+
 public class FlashcardsActivity extends AppCompatActivity {
-    private static int i=0;
-    private static int j=0;
+    private static int flashcardIterator = 0;
+    private static FlashcardSide flashcardSide = FRONT;
     public static final String TAG = "debuggingVocabulary";
     private static String language;
     private VocabularyDAO vocabularyDAO;
-    private CategoryDAO categoryDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashcards);
-      //  Log.d(TAG, "Flashcards");
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        vocabularyDAO=new VocabularyDAO(this);
-        categoryDAO=new CategoryDAO(this);
+        vocabularyDAO = new VocabularyDAO(this);
         loadContent();
     }
 
-    private void loadContent(){
+    private void loadContent() {
 
         final TextView flashcardWord = (TextView) findViewById(R.id.vocabulary_flashcard_textView);
         Button invertWordButton = (Button) findViewById(R.id.invertWord_button);
@@ -49,138 +49,140 @@ public class FlashcardsActivity extends AppCompatActivity {
         final SharedPreferences shared = getSharedPreferences("FlashcardsPreferences", 0);
         int categoryIdFromMain = shared.getInt("WhichIdFolder", 1);
 
-        Category categoryFromMain = categoryDAO.getFolder(categoryIdFromMain);
-
-        final List<Vocabulary> vocabularyFromData = vocabularyDAO.getAllVocabulary(categoryIdFromMain);
-
-
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(FlashcardsActivity.this);
         final String settingsDisplayType = sharedPref.getString("displayType", "");
         String settingsOrderType = sharedPref.getString("orderType", "");
 
+        final List<Vocabulary> vocabularyFromData = vocabularyDAO.getAllVocabulary(categoryIdFromMain);
         final Vocabulary[] wordsReloadedInRandom = reloadedTableInRandom(vocabularyFromData);
 
-        //which language first
-        if(settingsOrderType.equals("Najpierw pierwszy język")) language="first";
-        else language="second";
-
-        //first flashcard
-        if(language.equals("first")){
-            if(settingsDisplayType.equals("Losowo"))flashcardWord.setText(wordsReloadedInRandom[i].getFirstLanguageWord());
-            else flashcardWord.setText(vocabularyFromData.get(i).getFirstLanguageWord());
-        }
-        if(language.equals("second")){
-            if(settingsDisplayType.equals("Losowo"))flashcardWord.setText(wordsReloadedInRandom[i].getSecondLanguageWord());
-            else flashcardWord.setText(vocabularyFromData.get(i).getSecondLanguageWord());
-        }
+        setSettingTextOfAnOptionForFirstFlashcard(settingsDisplayType, settingsOrderType, flashcardWord, wordsReloadedInRandom, vocabularyFromData);
 
         nextWordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(FlashcardsActivity.this);
-                String settingsOrderType = sharedPref.getString("orderType", "");
-                String settingsDisplayType = sharedPref.getString("displayType", "");
-                ///////////////////////////////////////////////////
-
-                List<Vocabulary> vocabularies = new ArrayList<>();
-                int size = wordsReloadedInRandom.length;
-
-                /*if(settingsDisplayType.equals("Losowo")){
-                    for(int i=0; i<wordsReloadedInRandom.length; i++)
-                         vocabularies.add(wordsReloadedInRandom[i]);
-                }
-
-                else{
-                    for(int i=0; i<vocabularyFromData.size(); i++)
-                        vocabularies.add(vocabularyFromData.get(i));
-                } */
-
-                for(int i=0; i<size; i++){
-                    if(settingsDisplayType.equals("Losowo"))  vocabularies.add(wordsReloadedInRandom[i]);
-                    else vocabularies.add(vocabularyFromData.get(i));
-                }
-
-                ///////////////////////////////////////////////////
-
-                if(settingsOrderType.equals("Najpierw pierwszy język")) language="first";
-                if(settingsOrderType.equals("Najpierw drugi język")) language="second";
-
-                    i++;
-                    j = 0;
-                    if (i < vocabularies.size()) {
-                        if (language.equals("first"))
-                            flashcardWord.setText(vocabularies.get(i).getFirstLanguageWord());
-                        if (language.equals("second"))
-                            flashcardWord.setText(vocabularies.get(i).getSecondLanguageWord());
-                    }
-                    else
-                        Toast.makeText(FlashcardsActivity.this, "Koniec", Toast.LENGTH_LONG).show();
-
+                nextWordButtonOnClick(wordsReloadedInRandom, vocabularyFromData, flashcardWord);
             }
         });
 
         invertWordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(FlashcardsActivity.this);
-                String settingsOrderType = sharedPref.getString("orderType", "");
-
-                ///////////////////////////////////////////////////
-
-                List<Vocabulary> vocabularies = new ArrayList<>();
-
-                if(settingsDisplayType.equals("Losowo")){
-                    for(int i=0; i<wordsReloadedInRandom.length; i++)
-                        vocabularies.add(wordsReloadedInRandom[i]);
-                }
-
-                else {
-                    for(int i=0; i<vocabularyFromData.size(); i++)
-                        vocabularies.add(vocabularyFromData.get(i));
-                }
-
-
-                ///////////////////////////////////////////////////
-
-                if(settingsOrderType.equals("Najpierw pierwszy język")) language="first";
-                if(settingsOrderType.equals("Najpierw drugi język")) language="second";
-
-                    j++;
-                    if(i>=vocabularies.size())i=vocabularies.size()-1;
-                    //if (i < vocabularies.size()) {
-                        if (j % 2 != 0) {
-                            if(language=="first")flashcardWord.setText(vocabularies.get(i).getSecondLanguageWord());
-                            if(language=="second")flashcardWord.setText(vocabularies.get(i).getFirstLanguageWord());
-                        } else if (j % 2 == 0) {
-                            if(language=="first")flashcardWord.setText(vocabularies.get(i).getFirstLanguageWord());
-                            if(language=="second")flashcardWord.setText(vocabularies.get(i).getSecondLanguageWord());
-                        }
-
-                    //} else
-                    //    Toast.makeText(FlashcardsActivity.this, "Koniec", Toast.LENGTH_LONG).show();
+                invertWordButtonOnClick(wordsReloadedInRandom, vocabularyFromData, flashcardWord);
             }
         });
 
     }
 
-    public Vocabulary[] reloadedTableInRandom(List<Vocabulary> list){
-        List<Vocabulary> wordsList = new ArrayList<>();
-        for(int i=0; i<list.size(); i++){
-           wordsList.add(list.get(i));
+    private void invertWordButtonOnClick(Vocabulary[] wordsReloadedInRandom, List<Vocabulary> vocabularyFromData, TextView flashcardWord) {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(FlashcardsActivity.this);
+        String settingsDisplayType = sharedPref.getString("displayType", "");
+
+        List<Vocabulary> vocabularies = new ArrayList<>();
+
+        if (settingsDisplayType.equals(getResources().getString(R.string.random_option))) {
+            vocabularies.addAll(Arrays.asList(wordsReloadedInRandom));
+        } else {
+            vocabularies.addAll(vocabularyFromData);
         }
 
-        int size= wordsList.size();
+        setLanguageFirsOptInVariables();
+        flashcardSide = flashcardSide.changeSide();
+
+        if (flashcardIterator >= vocabularies.size()) flashcardIterator = vocabularies.size() - 1;
+
+        setTextInTextViewAfterInvert(flashcardWord, vocabularies.get(flashcardIterator));
+    }
+
+    private void nextWordButtonOnClick(Vocabulary[] wordsReloadedInRandom, List<Vocabulary> vocabularyFromData, TextView flashcardWord) {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(FlashcardsActivity.this);
+        String settingsDisplayType = sharedPref.getString("displayType", "");
+
+        List<Vocabulary> vocabularies = new ArrayList<>();
+        int size = wordsReloadedInRandom.length;
+
+        for (int i = 0; i < size; i++) {
+            if (settingsDisplayType.equals(getResources().getString(R.string.random_option)))
+                vocabularies.add(wordsReloadedInRandom[i]);
+            else vocabularies.add(vocabularyFromData.get(i));
+        }
+
+        setLanguageFirsOptInVariables();
+
+        flashcardIterator++;
+        flashcardSide = FRONT;
+
+        if (flashcardIterator < vocabularies.size()) {
+            if (language.equals("first"))
+                flashcardWord.setText(vocabularies.get(flashcardIterator).getFirstLanguageWord());
+            else flashcardWord.setText(vocabularies.get(flashcardIterator).getSecondLanguageWord());
+        } else
+            Toast.makeText(FlashcardsActivity.this, "Finish", Toast.LENGTH_LONG).show();
+    }
+
+    private void setLanguageFirsOptInVariables() {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(FlashcardsActivity.this);
+        String settingsOrderType = sharedPref.getString("orderType", "");
+        if (settingsOrderType.equals(getResources().getString(R.string.first_language_option)))
+            language = "first";
+        else language = "second";
+    }
+
+    private void setSettingTextOfAnOptionForFirstFlashcard(String settingsDisplayType, String settingsOrderType,
+                                                           TextView flashcardWord, Vocabulary[] wordsReloadedInRandom,
+                                                           List<Vocabulary> vocabularyFromData) {
+        //which language first
+        if (settingsOrderType.equals(getResources().getString(R.string.first_language_option)))
+            language = "first";
+        else language = "second";
+
+        //first flashcard
+        if (language.equals("first")) {
+            setTextInTextView(flashcardWord, settingsDisplayType, wordsReloadedInRandom[flashcardIterator].getFirstLanguageWord(), vocabularyFromData.get(flashcardIterator).getFirstLanguageWord());
+        }
+        if (language.equals("second")) {
+            setTextInTextView(flashcardWord, settingsDisplayType, wordsReloadedInRandom[flashcardIterator].getSecondLanguageWord(), vocabularyFromData.get(flashcardIterator).getSecondLanguageWord());
+        }
+    }
+
+    private void setTextInTextViewAfterInvert(TextView flashcardWord, Vocabulary vocabulary) {
+
+        String frontWord, backWord;
+
+        frontWord = language.equals("first") ? vocabulary.getSecondLanguageWord() : vocabulary.getFirstLanguageWord();
+        backWord = language.equals("second") ? vocabulary.getSecondLanguageWord() : vocabulary.getFirstLanguageWord();
+
+        if (flashcardSide == BACK)
+            flashcardWord.setText(frontWord);
+        else
+            flashcardWord.setText(backWord);
+    }
+
+    private void setTextInTextView(TextView flashcardWord, String settingsDisplayType,
+                                   String randomOptText, String sequenceOptText) {
+        if (settingsDisplayType.equals(getResources().getString(R.string.random_option)))
+            flashcardWord.setText(randomOptText);
+        else flashcardWord.setText(sequenceOptText);
+    }
+
+    public Vocabulary[] reloadedTableInRandom(List<Vocabulary> list) {
+        List<Vocabulary> wordsList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            wordsList.add(list.get(i));
+        }
+
+        int size = wordsList.size();
         Random generator = new Random();
-        Vocabulary [] table = new Vocabulary[size];
+        Vocabulary[] table = new Vocabulary[size];
         int range = size;
 
-        for (int i=0; i<size; i++){
-            int random=generator.nextInt(range);
-            table[i]=wordsList.get(random);
+        for (int i = 0; i < size; i++) {
+            int random = generator.nextInt(range);
+            table[i] = wordsList.get(random);
 
             Vocabulary temp = wordsList.get(random);
-            wordsList.set(random, wordsList.get(range-1));
-            wordsList.set(range-1, temp);
+            wordsList.set(random, wordsList.get(range - 1));
+            wordsList.set(range - 1, temp);
 
             range--;
         }
@@ -188,25 +190,21 @@ public class FlashcardsActivity extends AppCompatActivity {
         return table;
     }
 
+    private void resetVariables() {
+        flashcardSide = FRONT;
+        flashcardIterator = 0;
+    }
+
     @Override
-    public boolean onSupportNavigateUp(){
-        if(j%2!=0) j--;
-        j=0;
-        i=0;
-        Log.d(TAG, "i= "+String.valueOf(i));
-        Log.d(TAG, "j= "+String.valueOf(j));
+    public boolean onSupportNavigateUp() {
+        resetVariables();
         finish();
         return true;
     }
 
     @Override
     public void onBackPressed() {
-        if(j%2!=0) j--;
-        j=0;
-        i=0;
-        Log.d(TAG, "i= "+String.valueOf(i));
-        Log.d(TAG, "j= "+String.valueOf(j));
-
+        resetVariables();
         Intent back = new Intent(FlashcardsActivity.this, VocabularyListActivity.class);
         startActivity(back);
     }
@@ -220,20 +218,17 @@ public class FlashcardsActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.preferences:
-            {
+        switch (item.getItemId()) {
+            case R.id.preferences: {
                 Intent intent = new Intent();
-               // intent.setClassName(this,"com.example.justyna.flashcards.MyPreferenceActivity");
-                intent.setClassName(this,"com.example.justyna.flashcards.SettingsActivity");
+                intent.setClassName(this, "com.example.justyna.flashcards.SettingsActivity");
                 startActivity(intent);
                 return true;
             }
 
-            case R.id.refreshFlashcards:
-            {
+            case R.id.refreshFlashcards: {
                 Intent refresh = new Intent(FlashcardsActivity.this, FlashcardsActivity.class);
-                i=0; j=0;
+                resetVariables();
                 startActivity(refresh);
                 finish();
                 return true;
@@ -241,4 +236,15 @@ public class FlashcardsActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    enum FlashcardSide {
+        FRONT, BACK;
+
+        FlashcardSide changeSide() {
+            return this == FRONT ? BACK : FRONT;
+        }
+    }
 }
+
+
